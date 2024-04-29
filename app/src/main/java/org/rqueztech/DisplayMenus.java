37216115e24a39bc86d120e568b272cf406119b6
rqueztech <rqueztech@noreply.com>
2024-04-29 15:36:06 +0900

    Change display to properly reflect user input desired

--- app/src/main/java/org/rqueztech/DisplayMenus.java
+++ app/src/main/java/org/rqueztech/DisplayMenus.java
@@ -9,11 +9,11 @@ public class DisplayMenus {
         System.out.println("*******************************");
         System.out.println("*         Main Menu           *");
         System.out.println("*******************************");
-        System.out.println("* 1. Display Employees.      *");
-        System.out.println("* 2. Add Employee.           *");
-        System.out.println("* 3. Remove Employee.        *");
-        System.out.println("* 4. Update Employee.        *");
-        System.out.println("* 5. View Ex-Employees(rmvd) *");
+        System.out.println("* 1. Add Employee.            *");
+        System.out.println("* 2. Display Employees.       *");
+        System.out.println("* 3. Remove Employee.         *");
+        System.out.println("* 4. Update Employee.         *");
+        System.out.println("* 5. View Ex-Employees(rmvd)  *");
         System.out.println("*******************************");
     }
 }
