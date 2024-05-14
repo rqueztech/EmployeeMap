255903489eed5d92524543352aeccf86c3c357e3
rqueztech <rqueztech@noreply.com>
2024-05-14 11:47:29 +0900

    Add list to append to when writing out to CSV. Need to create one list that shares references to retain information as overwrites. Must create an append function.

--- app/src/main/java/org/rqueztech/EmployeeMap.java
+++ app/src/main/java/org/rqueztech/EmployeeMap.java
@@ -5,13 +5,17 @@ package org.rqueztech;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.List;
+import java.util.ArrayList;
 
 public class EmployeeMap {
 	private Map <String, Employee> employeeMap;
     public int empNo;
+    public CsvOperations csvOperations;
 
 	public EmployeeMap() {
 		this.employeeMap = new HashMap<String, Employee>();
+        this.csvOperations = new CsvOperations();
 	}
 
 	public Map<String, Employee> getEmployeeMap() {
@@ -19,11 +23,16 @@ public class EmployeeMap {
 	}
 
 	public void addEmployee(String first_name, String last_name, String email) {
+        List <String[]> rowToWrite = new ArrayList<>();
+
         System.out.println("Add Employee");
         
         String employee_id = String.valueOf(++empNo);
-
 		this.getEmployeeMap().put(employee_id , new Employee(first_name, last_name, email));
+        rowToWrite.add(this.getEmployeeMap().get(employee_id).asList());
+
+        this.csvOperations.writeCsvFile("Employee.csv", rowToWrite);
+
 	}
 
 	public void removeEmployee(String empId) {
